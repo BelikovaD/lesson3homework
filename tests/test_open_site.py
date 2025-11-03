a14b18be0ab21d94e541e9963f17d8b342a092a1
darab <dasha.belikova.1994@mail.ru>
2025-11-04 02:14:12 +0300

    update file

--- tests/test_open_site.py
+++ tests/test_open_site.py
@@ -1,10 +1,21 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture
+def driver():
+    opts = Options()
+    opts.add_argument("--headless=new")
+    opts.add_argument("--window-size=1280,900")
+    driver = webdriver.Chrome(options=opts)
+    yield driver
+
+    driver.quit()
 
 
 @pytest.mark.github
-def test_github_open():
-    driver = webdriver.Chrome()
+def test_github_open(driver):
     url = 'https://github.com/'
     driver.get(url)
     assert driver.title == 'GitHub · Change is constant. GitHub keeps you ahead. · GitHub'
@@ -12,11 +23,8 @@ def test_github_open():
 
 
 @pytest.mark.google
-def test_google_open():
-    driver = webdriver.Chrome()
+def test_google_open(driver):
     url1 = 'https://www.google.com/'
     driver.get(url1)
     assert driver.title == 'Google'
     assert driver.current_url == url1
-
-
